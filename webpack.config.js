40cb5a245262e084225b28d7e680e60f2b793e31
Mr.What <1061608626@qq.com>
2020-08-11 01:00:02 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,75 @@
  * @LastEditors: Mr.What
  * @LastEditTime: 2020-08-10 17:41:22
  */
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAassetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: './dist',
-    filename: 'bundle.js',
-    publicPath: '/assets/'
+    path: resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      //   options: {
+      //     fix: true
+      //   }
+      // },
+      {
+        test: /\.css$/,
+        use: [
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      }, {
+        test: /\.less$/,
+        use: [
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-preset-env')
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        // 移除空格
+        collapseWhitespace: true,
+        // 移除注释
+        removeComments: true
+      }
+    }),
+    // 分离css文件
+    new MiniCssExtractPlugin(),
+    // 压缩css
+    new OptimizeCssAassetsWebpackPlugin()
+  ],
+  mode: 'development',
+  devServer: {
+    contentBase: resolve(__dirname, 'dist'),
+    open: true,
+    hot: true,
+    compress: true,
+    port: 8081
   }
 }
