5e4cf6ed7c40f10dc7b794504df3cb047f0a4a72
Hongxin Huang <huanghongxin@sinorock.net>
2020-08-10 18:32:14 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Mr.What
+ * @Date: 2020-08-10 17:30:01
+ * @LastEditors: Mr.What
+ * @LastEditTime: 2020-08-10 18:29:26
+ */
+import * as THREE from '../node_modules/three/build/three.module.js'
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js'
+import { GLTFExporter } from '../node_modules/three/examples/jsm/exporters/GLTFExporter.js'
+
+let fileInput = document.getElementById('file')
+let exportBtn = document.getElementById('exportBtn')
+fileInput.addEventListener('change', event => {
+  if (event.target.files.length === 1) {
+    importer(URL.createObjectURL(event.target.files[0]))
+  }
+})
+exportBtn.addEventListener('click', () => {
+  exporter(scene).then(data => {
+    console.log(data)
+  })
+})
+
+let scene = null
+function importer (url) {
+  let loader = new GLTFLoader()
+  loader.load(url, data => {
+    scene = data.scene
+  })
+}
+
+function exporter (object) {
+  return new Promise((resolve, reject) => {
+    let blob = null
+    let exporter = new GLTFExporter()
+    exporter.parse(object, data => {
+      debugger
+      if (data instanceof ArrayBuffer) {
+        blob = new Blob([data], {
+          type: 'application/octet-stream'
+        })
+        resolve({blob, name: (object.name || object.uuid) + '.glb'})
+      } else {
+        let output = JSON.stringify(data, null, 2)
+        blob = new Blob([output], {
+          type: 'text/plain'
+        })
+        resolve({blob, name: (object.name || object.uuid) + '.gltf'})
+      }
+    })
+  })
+}
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Mr.What
+ * @Date: 2020-08-10 17:37:58
+ * @LastEditors: Mr.What
+ * @LastEditTime: 2020-08-10 17:41:22
+ */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+    publicPath: '/assets/'
+  }
+}
